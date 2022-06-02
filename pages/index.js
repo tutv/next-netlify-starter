402c4e0c645aae0bae5ee8868fd96d6d7c3fb750
Tu Tran <7255177+tutv@users.noreply.github.com>
2022-06-02 11:53:16 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,11 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+    
+        <div>
+          <a href="https://tutv.dev/hello-world?abc=1">Link with https</a>
+          <a href="demoo://hello-world?abc=1">Link with schema</a>
+        </div>
       </main>
 
       <Footer />

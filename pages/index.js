bff119e8f06f3cbe2bc4bbc391713cdefa996bf5
tutv <tutv@foobla.com>
2022-06-02 15:47:31 +0700

    test

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,30 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className="container">
+            <Head>
+                <title>Next.js Starter!</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-    
-        <div>
-          <a href="https://tutv.dev/hello-world?abc=1">Link with https</a>
-          <p></p>
-          <a href="demoo://hello-world?abc=1">Link with schema</a>
+            <main>
+                <Header title="Welcome to my app!"/>
+                <p className="description">
+                    Get started by editing <code>pages/index.js</code>
+                </p>
+
+                <div>
+                    <a href="https://tutv.dev/hello-world?abc=1">Link with https</a>
+                    <p></p>
+                    <a href="demoo://hello-world?abc=1">Link with schema</a>
+
+                    <p></p>
+                    <a href="/hello-word?from-domain=1">From main domain</a>
+                </div>
+            </main>
+
+            <Footer/>
         </div>
-      </main>
-
-      <Footer />
-    </div>
-  )
+    )
 }
